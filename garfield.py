87ec6b514bd894c4a8ff3a8b6f396d80611adf88
Robert Hawdon <mail@robertianhawdon.me.uk>
2016-03-29 18:52:26 +0000

    Fixed issues with Garfield

--- garfield.py
+++ garfield.py
@@ -3,6 +3,8 @@ import MySQLdb
 import feedparser
 import time
 import hashlib
+import re
+from datetime import datetime
 
 crontable = []
 crontable.append([300, "update_data"])
@@ -43,7 +45,9 @@ def update_data():
 
     result = feed.entries[0].summary_detail
 
-    comicdate = match = re.search(r'\d{4}-\d{2}-\d{2}', feed.entries[0].link)
+    comicdatematch = re.search(r'\d{4}-\d{2}-\d{2}', feed.entries[0].link)
+
+    comicdate = datetime.strptime(comicdatematch.group(), '%Y-%m-%d').date()
 
     link = "http://garfield.com/comic/" + str(comicdate)
 
