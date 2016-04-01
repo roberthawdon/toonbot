9e9f189ba250a5719a70af2af5b7a786d2c9ff94
roberthawdon <robert.hawdon@bedegaming.com>
2016-04-01 23:04:23 +0100

    Corrected typo, added a couple of new commands

--- lobby.py
+++ lobby.py
@@ -58,7 +58,11 @@ def process_message(data):
                         else:
                             enabledflag = ""
                         tablecomics.add_row([comics[0], enabledflag])
-                    outputs.append([data['channel'], "Here are a list of available comics:\n```" + str(tablecomics) + "```\nType the name of each comic you want to subscribe to as an individual message. Type the name again to unsubscribe."])
+                    outputs.append([data['channel'], "Here is a list of available comics:\n```" + str(tablecomics) + "```\nType the name of each comic you want to subscribe to as an individual message. Type the name again to unsubscribe."])
+                elif data['text'].startswith("feedback"):
+                    outputs.append([data['channel'], "Sorry, the feedback function is still being worked on."])
+                elif data['text'] == "help":
+                    outputs.append([data['channel'], "Type `list` to view a list of available comics. You can subscribe to a comic by sending its name. Once subscribed, you'll receive the latest comic in a few minutes. If you change your mind, you can also unsubscribe by sending its name again. The `list` command will be updated to reflect which comics you are currently subscribed to.\n I should not be added to public or private chat rooms, but in the event I am, I will not talk. I've been designed to only talk in direct messages."])
                 else:
                     cmd = "SELECT * FROM tbl_comics WHERE comicname = %s"
                     curs.execute(cmd, ([data['text']]))
