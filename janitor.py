5068db3db1ba3ecd3a7aaf3994cfcb114141b8cc
Robert Hawdon <mail@robertianhawdon.me.uk>
2016-08-07 15:36:48 +0100

    Moved janitor run time to database

--- janitor.py
+++ janitor.py
@@ -7,7 +7,31 @@ from datetime import datetime
 from checktime import runat
 from checktimezone import checktimezone
 
-scheduled = "02:05:30"
+mysqlserver = config["MYSQL_SERVER"]
+mysqluser = config["MYSQL_USER"]
+mysqlpass = config["MYSQL_PASS"]
+mysqldb = config["MYSQL_DB"]
+
+try:
+    conn = MySQLdb.Connection(mysqlserver, mysqluser, mysqlpass, mysqldb)
+    curs = conn.cursor()
+    conn.set_character_set('utf8')
+    curs.execute('SET NAMES utf8;')
+    curs.execute('SET CHARACTER SET utf8;')
+    curs.execute('SET character_set_connection=utf8;')
+    cmd = "SELECT value FROM tbl_system WHERE name = 'janitor_run'"
+    curs.execute(cmd)
+    result = curs.fetchall()
+    for preference in result:
+        scheduled = preference[0]
+    
+except curs.Error, e:
+
+    print "Error %d: %s" % (e.args[0], e.args[1])
+    sys.exit(1)
+
+# scheduled = "02:05:30" # May be reimplemented for if the database entry is unavailable.
+
 cron = 60
 
 crontable = []
