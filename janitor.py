af270c41f1485ed2362f242946ad64580fdec5f5
Robert Hawdon <mail@robertianhawdon.me.uk>
2016-11-13 17:57:04 +0000

    Fixed bug that required a bot restart for janitor time to be updated

--- janitor.py
+++ janitor.py
@@ -12,25 +12,28 @@ mysqluser = config["MYSQL_USER"]
 mysqlpass = config["MYSQL_PASS"]
 mysqldb = config["MYSQL_DB"]
 
-try:
-    conn = MySQLdb.Connection(mysqlserver, mysqluser, mysqlpass, mysqldb)
-    curs = conn.cursor()
-    conn.set_character_set('utf8')
-    curs.execute('SET NAMES utf8;')
-    curs.execute('SET CHARACTER SET utf8;')
-    curs.execute('SET character_set_connection=utf8;')
-    cmd = "SELECT value FROM tbl_system WHERE name = 'janitor_run'"
-    curs.execute(cmd)
-    result = curs.fetchall()
-    for preference in result:
-        scheduled = preference[0]
+def checkruntime():
+    try:
+        conn = MySQLdb.Connection(mysqlserver, mysqluser, mysqlpass, mysqldb)
+        curs = conn.cursor()
+        conn.set_character_set('utf8')
+        curs.execute('SET NAMES utf8;')
+        curs.execute('SET CHARACTER SET utf8;')
+        curs.execute('SET character_set_connection=utf8;')
+        cmd = "SELECT value FROM tbl_system WHERE name = 'janitor_run'"
+        curs.execute(cmd)
+        result = curs.fetchall()
+        for preference in result:
+            scheduled = preference[0]
 
-except curs.Error, e:
+        return scheduled
 
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
+    except curs.Error, e:
 
-# scheduled = "02:05:30" # May be reimplemented for if the database entry is unavailable.
+        print "Error %d: %s" % (e.args[0], e.args[1])
+        sys.exit(1)
+
+    # scheduled = "02:05:30" # May be reimplemented for if the database entry is unavailable.
 
 cron = 60
 
@@ -39,5 +42,6 @@ crontable.append([cron, "janitor"])
 outputs = []
 
 def janitor():
+    scheduled = checkruntime()
     if runat(scheduled, cron):
         checkaccount()
